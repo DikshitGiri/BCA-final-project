fc49d2ccfbf4be1010404e6e5cbc3f56f0f30afd
DikshitGiri <dikshitgiri42@gmail.com>
2023-07-12 00:52:25 +0545

    initial

---
+++ Frontend/src/pages/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+
+  
+function About() {
+    return (
+        <div>
+           <Navbar/> 
+         <p>This is about page and this one is done for testing</p>
+         <p>Our testing has succeeded</p>
+         <p>want to add second paragraph</p> 
+        </div>
+    );
+}
+
+export default About;
